38f07e40a941a0136f8dca8286f71303c1f1b042
Steven Weaver <sweaver@temple.edu>
2017-05-12 15:41:25 -0400

    beautifying code

--- src/helpers.js
+++ src/helpers.js
@@ -1,27 +1,27 @@
-var datamonkey_error_modal = function (msg) {
+var datamonkey_error_modal = function(msg) {
   $('#modal-error-msg').text(msg);
   $('#errorModal').modal();
 };
 
 function b64toBlob(b64, onsuccess, onerror) {
-    var img = new Image();
+  var img = new Image();
 
-    img.onerror = onerror;
+  img.onerror = onerror;
 
-    img.onload = function onload() {
-        var canvas = document.getElementById("hyphy-chart-canvas");
-        canvas.width = img.width;
-        canvas.height = img.height;
+  img.onload = function onload() {
+    var canvas = document.getElementById("hyphy-chart-canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
 
-        var ctx = canvas.getContext('2d');
-      	ctx.fillStyle = "#FFFFFF";
-				ctx.fillRect(0,0,canvas.width,canvas.height);
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-        canvas.toBlob(onsuccess);
-    };
+    canvas.toBlob(onsuccess);
+  };
 
-    img.src = b64;
+  img.src = b64;
 }
 
 var datamonkey_export_csv_button = function(data) {
@@ -72,7 +72,7 @@ var datamonkey_save_image = function(type, container) {
     }
 
     var styles = "",
-        styleSheets = doc.styleSheets;
+      styleSheets = doc.styleSheets;
 
     if (styleSheets) {
       for (var i = 0; i < styleSheets.length; i++) {
@@ -95,12 +95,12 @@ var datamonkey_save_image = function(type, container) {
       canvas.height = image.height;
       var context = canvas.getContext("2d");
       context.fillStyle = "#FFFFFF";
-      context.fillRect(0,0,image.width,image.height);
+      context.fillRect(0, 0, image.width, image.height);
       context.drawImage(image, 0, 0);
       var img = canvas.toDataURL("image/png");
       var pom = document.createElement('a');
       pom.setAttribute('download', 'image.png');
-      pom.href = canvas.toDataURL("image/png");     
+      pom.href = canvas.toDataURL("image/png");
       $("body").append(pom);
       pom.click();
       pom.remove();
@@ -121,7 +121,7 @@ var datamonkey_save_image = function(type, container) {
   svg.setAttribute("version", "1.1");
 
   var defsEl = document.createElement("defs");
-  svg.insertBefore(defsEl, svg.firstChild); 
+  svg.insertBefore(defsEl, svg.firstChild);
 
   var styleEl = document.createElement("style");
   defsEl.appendChild(styleEl);
@@ -147,21 +147,22 @@ var datamonkey_save_image = function(type, container) {
   var to_download = [doctype + source];
   var image_string = 'data:image/svg+xml;base66,' + encodeURIComponent(to_download);
 
-  if(type == "png") {
-		b64toBlob(image_string,
-				function(blob) {
+  if (type == "png") {
+    b64toBlob(image_string,
+      function(blob) {
 
-						var url = window.URL.createObjectURL(blob);
-						var pom = document.createElement('a');
-						pom.setAttribute('download', 'image.png');
-						pom.setAttribute('href', url);
-						$("body").append(pom);
-						pom.click();
-						pom.remove();
+        var url = window.URL.createObjectURL(blob);
+        var pom = document.createElement('a');
+        pom.setAttribute('download', 'image.png');
+        pom.setAttribute('href', url);
+        $("body").append(pom);
+        pom.click();
+        pom.remove();
 
-				}, function(error) {
-						// handle error
-				});
+      },
+      function(error) {
+        // handle error
+      });
   } else {
     var pom = document.createElement('a');
     pom.setAttribute('download', 'image.svg');
@@ -173,28 +174,28 @@ var datamonkey_save_image = function(type, container) {
 
 };
 
-var datamonkey_validate_date = function () {
+var datamonkey_validate_date = function() {
 
   // Check that it is not empty
-  if($(this).val().length === 0) {
+  if ($(this).val().length === 0) {
     $(this).next('.help-block').remove();
     $(this).parent().removeClass('has-success');
     $(this).parent().addClass('has-error');
 
     jQuery('<span/>', {
-          class: 'help-block',
-          text : 'Field is empty'
-      }).insertAfter($(this));
+      class: 'help-block',
+      text: 'Field is empty'
+    }).insertAfter($(this));
 
-  } else if(isNaN(Date.parse($(this).val()))) {
+  } else if (isNaN(Date.parse($(this).val()))) {
     $(this).next('.help-block').remove();
     $(this).parent().removeClass('has-success');
     $(this).parent().addClass('has-error');
 
     jQuery('<span/>', {
-          class: 'help-block',
-          text : 'Date format should be in the format YYYY-mm-dd'
-      }).insertAfter($(this));
+      class: 'help-block',
+      text: 'Date format should be in the format YYYY-mm-dd'
+    }).insertAfter($(this));
 
   } else {
     $(this).parent().removeClass('has-error');
@@ -206,7 +207,7 @@ var datamonkey_validate_date = function () {
 
 function datamonkey_get_styles(doc) {
   var styles = "",
-      styleSheets = doc.styleSheets;
+    styleSheets = doc.styleSheets;
 
   if (styleSheets) {
     for (var i = 0; i < styleSheets.length; i++) {
@@ -256,7 +257,7 @@ function datamonkey_convert_svg_to_png(image_string) {
     canvas.height = image.height;
     var context = canvas.getContext("2d");
     context.fillStyle = "#FFFFFF";
-    context.fillRect(0,0,image.width,image.height);
+    context.fillRect(0, 0, image.width, image.height);
     context.drawImage(image, 0, 0);
     var img = canvas.toDataURL("image/png");
 
@@ -310,7 +311,7 @@ function datamonkey_save_newick_tree(type) {
   var to_download = [doctype + source]
   var image_string = 'data:image/svg+xml;base66,' + encodeURIComponent(to_download);
 
-  if(type == "png") {
+  if (type == "png") {
     datamonkey_convert_svg_to_png(image_string)
   } else {
     var pom = document.createElement('a');
@@ -324,10 +325,10 @@ function datamonkey_save_newick_tree(type) {
 }
 
 function datamonkey_validate_email(email) {
-  if($(this).find("input[name='receive_mail']")[0].checked) {
+  if ($(this).find("input[name='receive_mail']")[0].checked) {
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if(regex.test($(this).find("input[name='mail']").val())) {
-       // Give them green. They like that.
+    if (regex.test($(this).find("input[name='mail']").val())) {
+      // Give them green. They like that.
       $(this).removeClass('has-error');
       $(this).addClass('has-success');
       $(this).next('.help-block').remove();
@@ -337,9 +338,9 @@ function datamonkey_validate_email(email) {
       $(this).removeClass('has-success');
       $(this).addClass('has-error');
       var span = jQuery('<span/>', {
-            class: 'help-block col-lg-9 pull-right',
-            text : 'Invalid Email'
-        }).insertAfter($(this));
+        class: 'help-block col-lg-9 pull-right',
+        text: 'Invalid Email'
+      }).insertAfter($(this));
     }
   } else {
     $(this).removeClass('has-error');
@@ -349,38 +350,36 @@ function datamonkey_validate_email(email) {
 
 }
 
-function datamonkey_describe_vector (vector, as_list) {
+function datamonkey_describe_vector(vector, as_list) {
 
-    vector.sort (d3.ascending);
+  vector.sort(d3.ascending);
 
-    var d = {'min' : d3.min (vector),
-             'max' : d3.max (vector),
-             'median' : d3.median (vector),
-             'Q1' : d3.quantile (vector, 0.25),
-             'Q3' : d3.quantile (vector, 0.75),
-             'mean': d3.mean (vector)};
+  var d = {
+    'min': d3.min(vector),
+    'max': d3.max(vector),
+    'median': d3.median(vector),
+    'Q1': d3.quantile(vector, 0.25),
+    'Q3': d3.quantile(vector, 0.75),
+    'mean': d3.mean(vector)
+  };
 
-    if (as_list) {
+  if (as_list) {
 
-        d = "<pre>Range  :" + d['min'] + "-" + d['max'] + "\n"
-            +    "IQR    :" + d['Q1'] + "-" + d['Q3'] + "\n"
-            +    "Mean   :" + d['mean'] + "\n"
-            +    "Median :" + d['median'] + "\n"
-            + "</pre>";
+    d = "<pre>Range  :" + d['min'] + "-" + d['max'] + "\n" + "IQR    :" + d['Q1'] + "-" + d['Q3'] + "\n" + "Mean   :" + d['mean'] + "\n" + "Median :" + d['median'] + "\n" + "</pre>";
 
-        /*d =
-        "<dl class = 'dl-horizontal'>" +
-        "<dt>Range</dt><dd>" + d['min'] + "-" + d['max'] + "</dd>" +
-        "<dt>IQR</dt><dd>" + d['Q1'] + "-" + d['Q3'] +  "</dd>" +
-        "<dt>Mean</dt><dd>" + d['mean'] +  "</dd>" +
-        "<dt>Median</dt><dd>" + d['median'] + "</dd></dl>";*/
-    }
+    /*d =
+    "<dl class = 'dl-horizontal'>" +
+    "<dt>Range</dt><dd>" + d['min'] + "-" + d['max'] + "</dd>" +
+    "<dt>IQR</dt><dd>" + d['Q1'] + "-" + d['Q3'] +  "</dd>" +
+    "<dt>Mean</dt><dd>" + d['mean'] +  "</dd>" +
+    "<dt>Median</dt><dd>" + d['median'] + "</dd></dl>";*/
+  }
 
-    return d;
+  return d;
 
 }
 
-function datamonkey_export_handler (data, filename, mimeType) {
+function datamonkey_export_handler(data, filename, mimeType) {
 
   function msieversion() {
     var ua = window.navigator.userAgent;
@@ -391,7 +390,7 @@ function datamonkey_export_handler (data, filename, mimeType) {
     return false;
   }
 
-  if(msieversion()) {
+  if (msieversion()) {
     var IEwindow = window.open();
     IEwindow.document.write(data);
     IEwindow.document.close();
@@ -399,7 +398,7 @@ function datamonkey_export_handler (data, filename, mimeType) {
     IEwindow.close();
   } else {
     var pom = document.createElement('a');
-    pom.setAttribute('href', 'data:' + (mimeType || 'text/plain')  +  ';charset=utf-8,' + encodeURIComponent(data));
+    pom.setAttribute('href', 'data:' + (mimeType || 'text/plain') + ';charset=utf-8,' + encodeURIComponent(data));
     pom.setAttribute('download', filename || "download.tsv");
     pom.click();
     pom.remove();
@@ -407,68 +406,81 @@ function datamonkey_export_handler (data, filename, mimeType) {
 
 }
 
-function datamonkey_table_to_text (table_id, sep) {
-    sep = sep || "\t";
-    var header_row = [];
-    d3.select (table_id + " thead").selectAll ("th").each (function () {header_row.push (d3.select(this).text())});
-    var data_rows = [];
-    d3.select (table_id + " tbody").selectAll ("tr").each (function (d,i) {data_rows.push ([]); d3.select (this).selectAll ("td").each (function () {data_rows[i].push (d3.select(this).text())})});
+function datamonkey_table_to_text(table_id, sep) {
+  sep = sep || "\t";
+  var header_row = [];
+  d3.select(table_id + " thead").selectAll("th").each(function() {
+    header_row.push(d3.select(this).text())
+  });
+  var data_rows = [];
+  d3.select(table_id + " tbody").selectAll("tr").each(function(d, i) {
+    data_rows.push([]);
+    d3.select(this).selectAll("td").each(function() {
+      data_rows[i].push(d3.select(this).text())
+    })
+  });
 
-    return header_row.join (sep) + "\n" +
-           data_rows.map (function (d) {return d.join (sep);}).join ("\n");
+  return header_row.join(sep) + "\n" +
+    data_rows.map(function(d) {
+      return d.join(sep);
+    }).join("\n");
 }
 
 function datamonkey_capitalize(s) {
-  if(s.length > 0) {
+  if (s.length > 0) {
     return s[0].toUpperCase() + s.slice(1);
   } else {
     return s;
   }
 }
 
-function datamonkey_count_partitions (json) {
-    try {
-      return _.keys (json).length;
-    } catch (e) {
-        // ignore errors
+function datamonkey_count_partitions(json) {
+  try {
+    return _.keys(json).length;
+  } catch (e) {
+    // ignore errors
+  }
+  return 0;
+}
+
+function datamonkey_sum(object, accessor) {
+  accessor = accessor || function(value) {
+    return value;
+  };
+  return _.reduce(object, function(sum, value, index) {
+    return sum + accessor(value, index);
+  }, 0);
+}
+
+function datamonkey_count_sites_from_partitions(json) {
+  try {
+    return datamonkey_sum(json["partitions"], function(value) {
+      return value["coverage"][0].length;
+    });
+  } catch (e) {
+    // ignore errors
+  }
+  return 0;
+}
+
+function datamonkey_filter_list(list, predicate, context) {
+  var result = {};
+  predicate = _.bind(predicate, context);
+  _.each(list, _.bind(function(value, key) {
+    if (predicate(value, key)) {
+      result[key] = value;
     }
-    return 0;
+  }, context));
+  return result;
 }
 
-function datamonkey_sum (object, accessor) {
-    accessor = accessor || function (value) {return value;};
-    return _.reduce (object, function (sum, value, index) {return sum + accessor (value, index);},0);
-}
-
-function datamonkey_count_sites_from_partitions (json) {
-    try {
-        return datamonkey_sum (json ["partitions"], function (value) {return value["coverage"][0].length;});
-    } catch (e) {
-        // ignore errors
-    }
-    return 0;
-}
-
-function datamonkey_filter_list (list, predicate, context) {
-    var result = {};
-    predicate = _.bind (predicate, context);
-    _.each (list, _.bind(function (value, key) {
-        if (predicate (value, key)) {
-            result[key] = value;
-        }
-      }, context)
-    );
-    return result;
-}
-
-function datamonkey_map_list (list, transform, context) {
-    var result = {};
-    transform = _.bind (transform, context);
-   _.each (list, _.bind(function (value, key) {
-        result[key] = transform(value,key);
-      }, context)
-    );
-    return result;
+function datamonkey_map_list(list, transform, context) {
+  var result = {};
+  transform = _.bind(transform, context);
+  _.each(list, _.bind(function(value, key) {
+    result[key] = transform(value, key);
+  }, context));
+  return result;
 }
 
 module.exports.errorModal = datamonkey_error_modal;
@@ -487,5 +499,5 @@ module.exports.capitalize = datamonkey_capitalize;
 module.exports.countPartitionsJSON = datamonkey_count_partitions;
 module.exports.countSitesFromPartitionsJSON = datamonkey_count_sites_from_partitions;
 module.exports.sum = datamonkey_sum;
-module.exports.filter=datamonkey_filter_list;
-module.exports.map=datamonkey_map_list;
+module.exports.filter = datamonkey_filter_list;
+module.exports.map = datamonkey_map_list;
--- src/hivtrace.js
+++ src/hivtrace.js
@@ -1,7 +1,15 @@
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
-import {clusterNetwork, graphSummary} from './clusternetwork.js';
-import {histogram, histogramDistances} from './histogram.js';
-import {scatterPlot} from './scatterplot.js';
+import {
+  clusterNetwork,
+  graphSummary
+} from './clusternetwork.js';
+import {
+  histogram,
+  histogramDistances
+} from './histogram.js';
+import {
+  scatterPlot
+} from './scatterplot.js';
 
 var misc = require('./misc.js');
 var helpers = require('./helpers.js');
