d8568b2de4332c0aac0dc6a5cc5069f68b4b2af4
Steven Weaver <sweaver@temple.edu>
2017-05-12 15:49:05 -0400

    Update entry.js

--- src/entry.js
+++ src/entry.js
@@ -4,9 +4,7 @@ require("font-awesome/css/font-awesome.css");
 require('./hivtrace.css');
 require('bootstrap');
 
-var hyphyvision = require('hyphy-vision');
 var hivtrace = require('./hivtrace.js');
 
 // Create new hyphy-vision export
 window.hivtrace = hivtrace;
-window.hyphyvision = hyphyvision;
