7dd5dfce55bdb3720069595194e4319e2aeac5c9
Steven Weaver <sweaver@temple.edu>
2022-11-23 13:13:37 -0600

    updating to webpack 5

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,12 @@
 window.jQuery = window.$ = $;
 
-require("font-awesome/css/font-awesome.css");
+require("@fortawesome/fontawesome-free/css/all.css");
+
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
+
 require("bootstrap");
 require("jspanel4/dist/jspanel.min.css");
 require("./hivtrace.css");
@@ -9,7 +15,7 @@ var hivtrace = require("./hivtrace.js");
 
 Math.log10 =
   Math.log10 ||
-  function(x) {
+  function (x) {
     return Math.log(x) / Math.LN10;
   };
 
