c235cb9fc460657992c905d5ba374222f746e9f4
Daniel Ji <jidaniel1234@gmail.com>
2024-06-17 22:10:47 -0700

    add node table sorting & filtering test

--- ui-tests/clusterOI.spec.js
+++ ui-tests/clusterOI.spec.js
@@ -1,20 +1,22 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
 let errors = [];
 
 test.beforeEach(async ({ page }) => {
   errors = [];
-  page.on('console', msg => {
-    if (msg.type() === 'error') {
+  page.on("console", (msg) => {
+    if (msg.type() === "error") {
       console.log(msg.text());
       errors.push(msg.text());
     }
-  })
+  });
 
-  await page.goto('http://127.0.0.1:8080/html/priority-sets-args.html?network=../ui-tests/data/network.json');
+  await page.goto(
+    "http://127.0.0.1:8080/html/priority-sets-args.html?network=../ui-tests/data/network.json"
+  );
 });
 
-test.afterEach(({ page }) => {
+test.afterEach(async ({ page }) => {
   expect(errors).toEqual([]);
 });
 
@@ -30,8 +32,8 @@ const getAcceptDialogFunction = (message) => {
     } else {
       await dialog.dismiss();
     }
-  }
-}
+  };
+};
 
 const openEditor = async (page) => {
   // go to clusterOI tab
@@ -43,12 +45,14 @@ const openEditor = async (page) => {
   await expect(jsPanels).toHaveLength(0);
 
   await expect(page.locator("#trace-priority-sets")).toBeVisible();
-  await expect(page.getByText("Create A Cluster of Interest", { exact: true })).toBeVisible();
+  await expect(
+    page.getByText("Create A Cluster of Interest", { exact: true })
+  ).toBeVisible();
   await page.getByText("Create A Cluster of Interest", { exact: true }).click();
 
   jsPanels = await page.locator(".jsPanel").all();
   await expect(jsPanels).toHaveLength(1);
-}
+};
 
 const createCluster = async (page, nodes, editorOpen = false) => {
   if (!editorOpen) {
@@ -56,7 +60,9 @@ const createCluster = async (page, nodes, editorOpen = false) => {
   }
 
   for (const node of nodes) {
-    await page.locator('[data-hivtrace-ui-role="priority-panel-nodeids"]').fill(node);
+    await page
+      .locator('[data-hivtrace-ui-role="priority-panel-nodeids"]')
+      .fill(node);
     await page.locator("#priority-panel-add-node").click();
   }
 
@@ -67,14 +73,20 @@ const createCluster = async (page, nodes, editorOpen = false) => {
   }
 
   await expect(nodeTable).toBeVisible();
-}
+};
 
 const previewClusterOI = async (page) => {
   await page.locator("#priority-panel-preview").click();
-  await expect(page.locator(".subcluster-view")
-    .filter({ has: page.getByText('Color: Cluster of Interest Status', { exact: true }) }))
-    .toBeVisible();
-}
+  await expect(
+    page
+      .locator(".subcluster-view")
+      .filter({
+        has: page.getByText("Color: Cluster of Interest Status", {
+          exact: true,
+        }),
+      })
+  ).toBeVisible();
+};
 
 /**
  * Assumes that the clusterOI editor is open
@@ -85,13 +97,17 @@ const saveClusterOI = async (page, name, expectDialog = false) => {
   await page.locator("#priority-panel-save").click();
   await expect(page.locator(".has-error")).toBeVisible();
 
-  await page.locator('[data-hivtrace-ui-role="priority-panel-name"]').fill(name);
+  await page
+    .locator('[data-hivtrace-ui-role="priority-panel-name"]')
+    .fill(name);
 
   if (expectDialog) {
-    const acceptDialog = getAcceptDialogFunction("This cluster of interest does not include all the nodes in the current");
-    await page.on('dialog', acceptDialog);
+    const acceptDialog = getAcceptDialogFunction(
+      "This cluster of interest does not include all the nodes in the current"
+    );
+    await page.on("dialog", acceptDialog);
     await page.locator("#priority-panel-save").click();
-    await page.off('dialog', acceptDialog);
+    await page.off("dialog", acceptDialog);
   } else {
     await page.locator("#priority-panel-save").click();
   }
@@ -104,40 +120,47 @@ const saveClusterOI = async (page, name, expectDialog = false) => {
 
   await page.locator("#priority-set-tab").click();
 
-  await expect(page.locator("#priority_set_table")
-    .filter({ has: page.getByText(name) }))
-    .toBeVisible();
+  await expect(
+    page.locator("#priority_set_table").filter({ has: page.getByText(name) })
+  ).toBeVisible();
 };
 
-
-test('clusterOI editor opens, can add nodes', async ({ page }) => {
-  // these specific nodes cause a confirm to appear when trying to save the clusterOI  
+test("clusterOI editor opens, can add nodes", async ({ page }) => {
+  // these specific nodes cause a confirm to appear when trying to save the clusterOI
   await createCluster(page, ["BMK384750US2015", "BMK385560US2007"]);
   await saveClusterOI(page, "Cluster 1", true);
 });
 
-test('preview cluster and then open clusterOI editor', async ({ page }) => {
-  await page.goto('http://127.0.0.1:8080/html/priority-sets-args.html?network=../ui-tests/data/network.json');
+test("preview cluster and then open clusterOI editor", async ({ page }) => {
+  await page.goto(
+    "http://127.0.0.1:8080/html/priority-sets-args.html?network=../ui-tests/data/network.json"
+  );
 
   await page.locator(".cluster-group").first().click();
-  await page.getByText("Show this cluster in separate tab", { exact: true }).click();
-  await expect(page.locator("#top_level_tab_container")
-    .filter({ has: page.getByText("Cluster 1") }))
-    .toBeVisible();
+  await page
+    .getByText("Show this cluster in separate tab", { exact: true })
+    .click();
+  await expect(
+    page
+      .locator("#top_level_tab_container")
+      .filter({ has: page.getByText("Cluster 1") })
+  ).toBeVisible();
 
   await createCluster(page, ["BMK384750US2015"]);
 
-  await previewClusterOI(page)
+  await previewClusterOI(page);
 });
 
-test('add nodes via graph to clusterOI editor and save', async ({ page }) => {
+test("add nodes via graph to clusterOI editor and save", async ({ page }) => {
   await openEditor(page);
 
   await page.locator("#trace-default-tab").click();
   await page.locator(".cluster-group").first().click();
-  await page.getByText("Add this cluster to the cluster of interest", { exact: true }).click();
+  await page
+    .getByText("Add this cluster to the cluster of interest", { exact: true })
+    .click();
 
-  // check that the nodes are added, empty clusterOI text shouldn't exist 
+  // check that the nodes are added, empty clusterOI text shouldn't exist
   await expect(page.getByText("clusterOI editor (0 nodes)")).toHaveCount(0);
 
   await previewClusterOI(page);
@@ -145,13 +168,17 @@ test('add nodes via graph to clusterOI editor and save', async ({ page }) => {
   await saveClusterOI(page, "Cluster 1");
 });
 
-test('add nodes via graph to clusterOI editor, save, clone clusterOI, save, and delete all', async ({ page }) => {
+test("add nodes via graph to clusterOI editor, save, clone clusterOI, save, and delete all", async ({
+  page,
+}) => {
   // add nodes via graph to clusterOI editor and save
   await openEditor(page);
 
   await page.locator("#trace-default-tab").click();
   await page.locator(".cluster-group").first().click();
-  await page.getByText("Add this cluster to the cluster of interest", { exact: true }).click();
+  await page
+    .getByText("Add this cluster to the cluster of interest", { exact: true })
+    .click();
 
   await previewClusterOI(page);
 
@@ -159,7 +186,11 @@ test('add nodes via graph to clusterOI editor, save, clone clusterOI, save, and 
 
   // clone clusterOI and save
   await page.locator(".view-edit-cluster").first().click();
-  await page.getByText("Clone this cluster of interest in a new editor panel", { exact: true }).click();
+  await page
+    .getByText("Clone this cluster of interest in a new editor panel", {
+      exact: true,
+    })
+    .click();
 
   const jsPanels = await page.locator(".jsPanel").all();
   await expect(jsPanels).toHaveLength(1);
@@ -168,34 +199,51 @@ test('add nodes via graph to clusterOI editor, save, clone clusterOI, save, and 
 
   await saveClusterOI(page, "Cluster 2", true);
 
-  await expect(await page.locator("#priority_set_table")
-    .filter({ has: page.getByText("Cluster 1") })
-    .filter({ has: page.getByText("Cluster 2") }))
-    .toBeVisible();
+  await expect(
+    await page
+      .locator("#priority_set_table")
+      .filter({ has: page.getByText("Cluster 1") })
+      .filter({ has: page.getByText("Cluster 2") })
+  ).toBeVisible();
 
   // delete all clusters, accept confirmation dialog
   await page.locator(".view-edit-cluster").first().click();
-  const acceptDialog = getAcceptDialogFunction("This action cannot be undone. Proceed?")
-  await page.on('dialog', acceptDialog);
-  await page.getByText("Delete this cluster of interest", { exact: true }).first().click();
-  await page.off('dialog', acceptDialog);
+  const acceptDialog = getAcceptDialogFunction(
+    "This action cannot be undone. Proceed?"
+  );
+  await page.on("dialog", acceptDialog);
+  await page
+    .getByText("Delete this cluster of interest", { exact: true })
+    .first()
+    .click();
+  await page.off("dialog", acceptDialog);
 
-  await expect(await page.locator("#priority_set_table")
-    .filter({ has: page.getByText("Cluster 1") })
-    .filter({ has: page.getByText("Cluster 2") }))
-    .toHaveCount(0);
+  await expect(
+    await page
+      .locator("#priority_set_table")
+      .filter({ has: page.getByText("Cluster 1") })
+      .filter({ has: page.getByText("Cluster 2") })
+  ).toHaveCount(0);
 
-  await expect(await page.locator("#priority_set_table")
-    .filter({ has: page.getByText("Cluster 2") }))
-    .toHaveCount(1);
+  await expect(
+    await page
+      .locator("#priority_set_table")
+      .filter({ has: page.getByText("Cluster 2") })
+  ).toHaveCount(1);
 
   // delete the last cluster
   await page.locator(".view-edit-cluster").first().click();
-  const acceptDialog2 = getAcceptDialogFunction("This action cannot be undone. Proceed?")
-  await page.on('dialog', acceptDialog2);
-  await page.getByText("Delete this cluster of interest", { exact: true }).first().click();
-  await page.off('dialog', acceptDialog2);
+  const acceptDialog2 = getAcceptDialogFunction(
+    "This action cannot be undone. Proceed?"
+  );
+  await page.on("dialog", acceptDialog2);
+  await page
+    .getByText("Delete this cluster of interest", { exact: true })
+    .first()
+    .click();
+  await page.off("dialog", acceptDialog2);
 
-  await expect(await page.locator("#priority_set_table tbody tr"))
-    .toHaveCount(0);
-})
+  await expect(await page.locator("#priority_set_table tbody tr")).toHaveCount(
+    0
+  );
+});
--- ui-tests/tables.spec.js
+++ ui-tests/tables.spec.js
@@ -1,30 +1,34 @@
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
 let errors = [];
 
 test.beforeEach(({ page }) => {
   errors = [];
-  page.on('console', msg => {
-    if (msg.type() === 'error') {
+  page.on("console", (msg) => {
+    if (msg.type() === "error") {
       console.log(msg.text());
       errors.push(msg.text());
     }
-  })
+  });
 });
 
-test.afterEach(({ page }) => {
+test.afterEach(async ({ page }) => {
   expect(errors).toEqual([]);
 });
 
-test('cluster table loads and works', async ({ page }) => {
-  await page.goto('http://127.0.0.1:8080/');
+test("cluster table loads and works", async ({ page }) => {
+  await page.goto("http://127.0.0.1:8080/");
 
   await expect(page.locator("#clusters-tab")).toBeVisible();
   await page.locator("#clusters-tab").click();
 
   await expect(page.locator("#trace-clusters")).toBeVisible();
-  await expect(page.getByText("Genotypes past 2 mo", { exact: true })).toBeVisible();
-  await expect(page.getByText("0.0071 [0.0071, 0.0071 - 0.0071]")).toBeVisible();
+  await expect(
+    page.getByText("Genotypes past 2 mo", { exact: true })
+  ).toBeVisible();
+  await expect(
+    page.getByText("0.0071 [0.0071, 0.0071 - 0.0071]")
+  ).toBeVisible();
 
   await expect(page.locator('[data-cluster="5"]')).toBeVisible();
   await page.locator('[data-cluster="5"]').click();
@@ -32,40 +36,168 @@ test('cluster table loads and works', async ({ page }) => {
   await expect(page.getByText("Listing nodes Cluster 5")).toBeVisible();
   await expect(page.getByText("GA00HHARS002054*").first()).toBeVisible();
 
-  await expect(await page.locator('.dl-horizontal').filter({ has: page.getByText("GA00HHARS002054*") }).count()).toBeGreaterThan(1);
+  await expect(
+    await page
+      .locator(".dl-horizontal")
+      .filter({ has: page.getByText("GA00HHARS002054*") })
+      .count()
+  ).toBeGreaterThan(1);
 
-  await page.locator('[data-hivtrace-ui-role="cluster_list_view_toggle"]').click();
+  await page
+    .locator('[data-hivtrace-ui-role="cluster_list_view_toggle"]')
+    .click();
 
-  await expect(await page.locator('.dl-horizontal').filter({ has: page.getByText("GA00HHARS002054*") }).count()).toBe(1);
-
+  await expect(
+    await page
+      .locator(".dl-horizontal")
+      .filter({ has: page.getByText("GA00HHARS002054*") })
+      .count()
+  ).toBe(1);
 });
 
-test('subclusters table loads and works', async ({ page }) => {
-  await page.goto('http://localhost:8080/html/priority-sets-args.html?network=../ui-tests/data/network.json');
+test("subclusters table loads and works", async ({ page }) => {
+  await page.goto(
+    "http://localhost:8080/html/priority-sets-args.html?network=../ui-tests/data/network.json"
+  );
 
   await expect(page.locator("#subclusters-tab")).toBeVisible();
   await page.locator("#subclusters-tab").click();
 
   await expect(page.locator("#trace-subclusters")).toBeVisible();
-  await expect(page.getByText("Cases dx within 12 months", { exact: true })).toBeVisible();
+  await expect(
+    page.getByText("Cases dx within 12 months", { exact: true })
+  ).toBeVisible();
   await expect(page.getByText("109.1", { exact: true })).toBeVisible();
 
   await page.locator('[data-cluster="4.1"]').click();
   await expect(page.getByText("Listing nodes Cluster 4.1")).toBeVisible();
   await expect(page.getByText("BMN090749DE2018").first()).toBeVisible();
-})
+});
 
-test('node table loads and works', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
+test("node table loads and works", async ({ page }) => {
+  await page.goto("http://localhost:8080/");
   await expect(page.locator("#nodes-tab")).toBeVisible();
   await page.locator("#nodes-tab").click();
 
   await expect(page.locator("#trace-nodes")).toBeVisible();
   await expect(page.getByText("# of links", { exact: true })).toBeVisible();
-  await expect(page.getByText("GA01HAGUS002656*")).toBeVisible();  
+  await expect(page.getByText("GA01HAGUS002656*")).toBeVisible();
 
   await page.getByText("view cluster").first().click();
   await expect(page.getByText("Cluster 1")).toBeVisible();
   await expect(await page.locator("#hivtrace-export-image").count()).toBe(2);
   await expect(page.locator("#hivtrace-export-image").last()).toBeVisible();
-})
+});
+
+test("node table sorting and filtering works", async ({ page }) => {
+  // sorting
+  await page.goto(
+    "http://localhost:8080/html/priority-sets-args.html?network=../ui-tests/data/network.json"
+  );
+  await expect(page.locator("#nodes-tab")).toBeVisible();
+  await page.locator("#nodes-tab").click();
+
+  await page.locator("#hivtrace_node_column_hiv_aids_dx_dt_3").click();
+  await page
+    .locator("[aria-labelledby='hivtrace_node_column_hiv_aids_dx_dt_3']")
+    .locator("li a")
+    .getByText("age_dx")
+    .click();
+  await page
+    .locator("#node_table")
+    .locator("tbody tr")
+    .nth(3)
+    .filter({ has: page.getByText("BMK384244US2007") })
+    .filter({ has: page.getByText("87") })
+    .filter({ has: page.getByText("Asian") });
+
+  await page
+    .locator("[title='age_dx']")
+    .locator("[data-column-id='3']")
+    .click();
+  await page
+    .locator("#node_table")
+    .locator("tbody tr")
+    .nth(3)
+    .filter({ has: page.getByText("BMK385331US2015") })
+    .filter({ has: page.getByText("13-19") })
+    .filter({ has: page.getByText("American Indian/Alaska Native") });
+
+  await page
+    .locator("[title='age_dx']")
+    .locator("[data-column-id='3']")
+    .click();
+  await expect(
+    page.locator("#trace-nodes").locator(".fa-sort-amount-desc")
+  ).toHaveCount(1);
+  await page
+    .locator("#node_table")
+    .locator("tbody tr")
+    .nth(3)
+    .filter({ has: page.getByText("BMN090524US2018") })
+    .filter({ has: page.getByText("60") })
+    .filter({ has: page.getByText("Hispanic/Latino") });
+
+  await page.locator("#hivtrace_node_column_age_dx_3").click();
+  await page
+    .locator("[aria-labelledby='hivtrace_node_column_age_dx_3']")
+    .locator("li a")
+    .getByText("hiv_aids_dx_dt")
+    .click();
+  await page
+    .locator("[title='Node ID']")
+    .locator("[data-column-id='0']")
+    .click();
+  await page
+    .locator("#node_table")
+    .locator("tbody tr")
+    .nth(3)
+    .filter({ has: page.getByText("-MN467202US2016") })
+    .filter({ has: page.getByText("Heterosexual Contact-Male") })
+    .filter({ has: page.getByText("2022-04-15") });
+
+  await expect(
+    page.locator("#trace-nodes").locator(".fa-sort-amount-desc")
+  ).toHaveCount(0);
+  await expect(
+    page.locator("#trace-nodes").locator(".fa-sort-amount-asc")
+  ).toHaveCount(1);
+
+  // filtering
+  await page
+    .locator("#trace-nodes thead")
+    .locator("[title='hiv_aids_dx_dt']")
+    .locator("a")
+    .filter({
+      has: page.locator(".fa-search"),
+    })
+    .click();
+  // wait because fill doesn't work immediately
+  await page.waitForTimeout(500);
+  await page
+    .locator("[data-hivtrace-ui-role='table-filter-term']")
+    .fill("20220101:20220201");
+  await page.waitForTimeout(500);
+  await page.keyboard.press("Enter");
+  await expect(
+    page
+      .locator("#node_table")
+      .locator("tbody tr")
+      .locator("visible=true")
+      .filter({
+        has: page.getByText("2022-04-15"),
+      })
+  ).toHaveCount(0);
+
+  await page.locator('[data-hivtrace-ui-role="table-filter-reset"]').click();
+  await expect(
+    await page
+      .locator("#node_table")
+      .locator("tbody tr")
+      .locator("visible=true")
+      .filter({
+        has: page.getByText("2022-04-15"),
+      })
+      .count()
+  ).toBeGreaterThan(0);
+});
