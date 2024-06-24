5ff685d6818568c5dcc23bf45d885a70b1761c82
Daniel Ji <daji@ucsd.edu>
2024-06-24 00:21:02 -0700

    fix indentation

--- ui-tests/general.spec.js
+++ ui-tests/general.spec.js
@@ -7,31 +7,32 @@ test.beforeEach(({ page }) => {
   errors = [];
   page.on('console', msg => {
     if (msg.type() === 'error') {
+
       errors.push(msg.text());
     }
   })
-	page.on("pageerror", (err) => {
+  page.on("pageerror", (err) => {
     errors.push(err.message);
-  }) 
+  })
 });
 
 test.afterEach(({ page }) => {
-	expect(errors).toEqual([]);
+  expect(errors).toEqual([]);
 });
 
 test('network graph loaded', async ({ page }) => {
-	await page.goto('http://127.0.0.1:8080/');
+  await page.goto('http://127.0.0.1:8080/');
 
-	await expect(page).toHaveTitle('HIV-TRACE');
-	await expect(page.locator("#hiv-trace-network-svg")).toBeVisible();
+  await expect(page).toHaveTitle('HIV-TRACE');
+  await expect(page.locator("#hiv-trace-network-svg")).toBeVisible();
 });
 
 test('network statistics loaded', async ({ page }) => {
-	await page.goto('http://127.0.0.1:8080/');
+  await page.goto('http://127.0.0.1:8080/');
 
-	await expect(page.locator("#graph-tab")).toBeVisible();
-	await page.locator("#graph-tab").click();
-	await expect(page.locator("#trace-graph")).toBeVisible();
-	await expect(page.getByText("Sequences used to make links")).toBeVisible();
-	await expect(page.getByText("0.891%")).toBeVisible();
+  await expect(page.locator("#graph-tab")).toBeVisible();
+  await page.locator("#graph-tab").click();
+  await expect(page.locator("#trace-graph")).toBeVisible();
+  await expect(page.getByText("Sequences used to make links")).toBeVisible();
+  await expect(page.getByText("0.891%")).toBeVisible();
 });
